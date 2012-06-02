39e60cc0f0e3a7d002ebd03a431f2b3711d404de
Josef Pacula <josefpacula@gmail.com>
2012-06-02 21:54:28 +0200

    added .m file for classes - but not functionwrapper and not correct name

--- Source/Modules/matlab.cxx
+++ Source/Modules/matlab.cxx
@@ -17,6 +17,15 @@ protected:
   bool ClassNameList_add(String* cppName, String* matlabName);
   String* ClassNameList_getMatlabName(String* cppName);
   void ClassNameList_print();
+	
+
+  File * f_begin;
+  String * f_runtime;
+  String * f_init;
+  String * f_header;
+  String * f_wrappers;
+
+  String * mClass_content;
 
 public:
 
@@ -34,7 +43,7 @@ bool MATLAB::ClassNameList_add(String* cppName, String* matlabName) {
     if(temp_ptr == 0)
       return false;
     ClassNameList.list = (ClassNameItem*) temp_ptr;
-    ClassNameList.capacity = 2*ClassNameList.capacity;
+    ClassNameList.capacity *= 2;
   }
   ClassNameItem newClassNameItem;
   newClassNameItem.cppName = cppName;
@@ -63,44 +72,49 @@ void MATLAB::ClassNameList_print() {
 int MATLAB::top(Node *n) {
 
    /* Initialize I/O */
-//   f_begin = NewFile(outfile, "w", SWIG_output_files());
-//   if (!f_begin) {
-//      FileErrorDisplay(outfile);
-//      SWIG_exit(EXIT_FAILURE);
-//   }
-//   f_runtime = NewString("");
-//   f_init = NewString("");
-//   f_header = NewString("");
-//   f_wrappers = NewString("");
-//
-//   /* Register file targets with the SWIG file handler */
-//   Swig_register_filebyname("begin", f_begin);
-//   Swig_register_filebyname("header", f_header);
-//   Swig_register_filebyname("wrapper", f_wrappers);
-//   Swig_register_filebyname("runtime", f_runtime);
-//   Swig_register_filebyname("init", f_init);
-//
-//   /* Output module initialization code */
-//   Swig_banner(f_begin);
+    
+    //fill outfile with out file name
+    String *outfile=NewString(Getattr(n,"name"));
+    Append(outfile,"wrap.cpp"); //toto neni spravne, dodelam az budu v Brne - zatim tento hack
+
+    f_begin = NewFile(outfile, "w", SWIG_output_files());
+    if (!f_begin) {
+       FileErrorDisplay(outfile);
+       SWIG_exit(EXIT_FAILURE);
+    }
+    f_runtime = NewString("");
+    f_init = NewString("");
+    f_header = NewString("");
+    f_wrappers = NewString("");
+
+   /* Register file targets with the SWIG file handler */
+    Swig_register_filebyname("begin", f_begin);
+    Swig_register_filebyname("header", f_header);
+    Swig_register_filebyname("wrapper", f_wrappers);
+    Swig_register_filebyname("runtime", f_runtime);
+    Swig_register_filebyname("init", f_init);
+
+   /* Output module initialization code */
+    Swig_banner(f_begin);
 
 
-  /* Emit code for children */
-  Language::top(n);
+   /* Emit code for children */
+    Language::top(n);
 
 
-//   /* Write all to the file */
-//   Dump(f_runtime, f_begin);
-//   Dump(f_header, f_begin);
-//   Dump(f_wrappers, f_begin);
-//   Wrapper_pretty_print(f_init, f_begin);
-//
-//   /* Cleanup files */
-//   Delete(f_runtime);
-//   Delete(f_header);
-//   Delete(f_wrappers);
-//   Delete(f_init);
-//   Close(f_begin);
-//   Delete(f_begin);
+    /* Write all to the file */
+    Dump(f_runtime, f_begin);
+    Dump(f_header, f_begin);
+    Dump(f_wrappers, f_begin);
+    Wrapper_pretty_print(f_init, f_begin);
+ 
+    /* Cleanup files */
+    Delete(f_runtime);
+    Delete(f_header);
+    Delete(f_wrappers);
+    Delete(f_init);
+    Close(f_begin);
+    Delete(f_begin);
 
   return SWIG_OK;
 }
@@ -130,6 +144,24 @@ void MATLAB::main(int argc, char *argv[]) {
 int MATLAB::classHandler(Node* n) {
   String   *name   = Getattr(n,"sym:name");
   Printf(stderr,"classWrapper   : %s\n", name);
+
+  String * mClass_fileName = NewString("jmenoTridy.m"); //?
+  File * mClass_file = NewFile(mClass_fileName, "w", SWIG_output_files());
+  if (!mClass_file) {
+     FileErrorDisplay(mClass_file);
+     SWIG_exit(EXIT_FAILURE);
+  }
+
+  mClass_content=NewString(""); //sem se zapise mfile pro tridu
+
+  //Language::classHandler(n); //poresi nam ruzne gety a sety a konstruktory atp
+
+  Dump(mClass_content, mClass_file);
+  Close(mClass_file);
+  Delete(mClass_content);
+  Delete(mClass_file);
+  Delete(mClass_fileName);
+
 }
 
 
@@ -141,6 +173,13 @@ int MATLAB::functionWrapper(Node *n) {
   String   *parmstr= ParmList_str_defaultargs(parms); // to string
   String   *func   = SwigType_str(type, NewStringf("%s(%s)", name, parmstr));
   String   *action = Getattr(n,"wrap:action");
+/*
+  File * mClass_file = NewFile(mClass_fileName, "w", SWIG_output_files());
+  if (!mClass_file) {
+     FileErrorDisplay(mClass_file);
+     SWIG_exit(EXIT_FAILURE);
+  }
+*/
 
   Printf(stderr,"functionWrapper   : %s\n", func);
   Printf(stderr,"           action : %s\n", action);
